9d128787d228af51a9f22acdef6824be776e7cdf
Joseph Ogbu <57672199+fallyfox@users.noreply.github.com>
2022-08-23 15:14:59 +0100

    Add files via upload

---
+++ polls-data.js
@@ -0,0 +1,38 @@
+const data = {
+    nana:{
+        details:'Nana Addo Dankwa Akufo-Addo is a Ghanaian politician who has served as the president of Ghana since 7 January 2017. In 2020, he was re-elected for his second term, which will end on 6 January 2025.',
+        origin:'Ghana',
+        profileUrl:'leaders/Nana Akufo-Addo.jpg',
+        noOfVotes:0
+    },
+    boris:{
+        details:'Alexander Boris de Pfeffel Johnson is a British politician who has served as Prime Minister of the United Kingdom and Leader of the Conservative Party since 2019.',
+        origin:'United Kingdom',
+        profileUrl:'leaders/Boris_Johnson.jpg',
+        noOfVotes:0
+    },
+    buhari:{
+        details:"Muhammadu Buhari GCFR is a Nigerian politician, who has served as the president of Nigeria since 2015. He is a retired Nigerian Army major general who served as the country's military head of state from 31 December 1983 to 27 August 1985, after taking power in a military coup d'état.",
+        origin:'Nigeria',
+        profileUrl:'leader/m-buhari.jpg',
+        noOfVotes:0
+    },
+    paul:{
+        details:'Paul Kagame is a Rwandan politician and former military officer. He is the fourth and current president of Rwanda, having taken office in 2000.',
+        origin:'Rwanda',
+        profileUrl:'leaders/Paul_Kagame.jpg',
+        noOfVotes:0
+    },
+    putin:{
+        details:'Vladimir Vladimirovich Putin is a Russian politician and former intelligence officer who has been serving as the president of Russia since 2012, having previously served between 2000 and 2008. He was the prime minister of Russia from 1999 to 2000 and again from 2008 to 2012.',
+        origin:'Russia',
+        profileUrl:'leaders/Vladimir_Putin.jpg',
+        noOfVotes:0
+    },
+    lapid:{
+        details:'Yair Lapid is an Israeli politician and former journalist who has been serving as the 14th prime minister of Israel since 1 July 2022. He previously served as the alternate prime minister of Israel and minister of Foreign Affairs from 2021 to 2022.',
+        origin:'Israel',
+        profileUrl:'leader/Yair_Lapid.jpg',
+        noOfVotes:0
+    },
+}
